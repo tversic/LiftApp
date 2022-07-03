93d5754cf7461dd6d19a5f32abb17f27473b299b
versict <tome.versic@autodesk.com>
2022-07-03 16:31:51 +0200

    Initial commit

---
+++ src/main/java/com/liftapp/LiftApp.java
@@ -0,0 +1,13 @@
+package com.liftapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiftApp.class, args);
+    }
+
+}
---
+++ src/test/java/com/liftapp/ZavrsniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liftapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZavrsniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
